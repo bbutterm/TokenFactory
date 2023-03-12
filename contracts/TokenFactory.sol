d8783a9e817995f63ae66b0bcb7a4f63a9759ba3
bbutterm <54075794+bbutterm@users.noreply.github.com>
2023-03-12 19:22:29 +0300

    Update TokenFactory.sol

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -3,24 +3,21 @@ pragma solidity ^0.8.17;
 import "./NewToken.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TokenFactory {
-    mapping(address => address) contractToOwner;
+contract TokenFactory{
+    mapping(address=>address[]) public ownerToContracts;
     address[] contractByID;
+    event NewTokenCreated(address _tokenAddr,address _owner, string indexed  _TokenName,string indexed _TokenSymb);
+    uint count = 0;
 
-    function createNewToken(
-        string memory _name,
-        string memory _symb,
-        uint256 _ownerTokens,
-        uint256 _systemTokens
-    ) public returns (address) {
-        NewToken newtoken = new NewToken(
-            _name,
-            _symb,
-            _ownerTokens,
-            _systemTokens
-        );
-        contractToOwner[msg.sender] = address(newtoken);
+    function createNewToke(string memory _name,string memory _symb,uint _ownerTokens,uint _systemTokens) public returns(address) {
+        count+=1;
+        NewToken newtoken = new NewToken(_name,_symb,_ownerTokens,_systemTokens,count);
+        ownerToContracts[msg.sender].push(address(newtoken));
         contractByID.push(address(newtoken));
-        return (address(newtoken));
+        emit NewTokenCreated(address(newtoken),msg.sender,_name,_symb);
+        return(address(newtoken));
+    }
+    function getAddr(address _owner) public view returns(address[] memory){
+        return(ownerToContracts[_owner]);
     }
 }
