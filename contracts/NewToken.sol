9d8780b582259943d59b073526f672a1fbc64ee6
bbutterm <54075794+bbutterm@users.noreply.github.com>
2023-03-15 13:54:57 +0300

    Update NewToken.sol

--- contracts/NewToken.sol
+++ contracts/NewToken.sol
@@ -1,4 +1,7 @@
-contract NewToken is ERC20, ERC20Burnable{
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract NewToken is ERC20{
     uint stakeID = 0;
     address public owner;
     mapping (address=>uint) public lockedTokens;
